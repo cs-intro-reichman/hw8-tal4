b358f0f9c38ab8624a5dc39f3a28ca7c3273d6b7
Tal Vered <tal.vered@post.runi.ac.il>
2025-01-05 17:20:01 +0200

    NETWORK_ADD_FOLLOWEE

--- Network.java
+++ Network.java
@@ -56,9 +56,12 @@ public class Network {
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
+        if(name1 == null || name2 == null) {
+            return false;
+        }
         User user1 = getUser(name1);
         User user2 = getUser(name2);
-        if (user1 == null || user2 == null) {
+        if (user1 == null || user2 == null || user1 == user2) {
             return false;
         }
         if (user1.follows(name2)) {
